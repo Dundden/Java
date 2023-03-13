f84982bf09400ad6511c0c9b3f5b830018ea3dbe
Dundden <127776867+Dundden@users.noreply.github.com>
2023-03-13 15:23:45 -0300

    ejercicio 1

---
+++ conceptos_basicos/capitulo1.java
@@ -0,0 +1,23 @@
+
+package conceptos_basicos;
+import java.util.*;
+
+public class capitulo1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int n;
+		
+		System.out.println("Ingrese un numero");
+		
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		
+		
+		System.out.println("El numero que usted ingreso es " + n);
+	}
+
+}
+
